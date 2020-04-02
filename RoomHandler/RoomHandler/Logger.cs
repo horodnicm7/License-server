32c88deb885af8796c7aeb35971804536926e8ff
Marian Horodnic <horodnicm7@gmail.com>
2020-04-02 17:07:51 +0300

    Created logger class and new gitignore rules

---
+++ RoomHandler/RoomHandler/Logger.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public class Logger {
+    public static void print(string message) {
+        Console.WriteLine(String.Format("[{0:HH:mm:ss tt}]: {1}", DateTime.Now, message));
+    }
+}
