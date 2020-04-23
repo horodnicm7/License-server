64fcda895bb4f9c183ff2428253141f3f157792f
Marian Horodnic <horodnicm7@gmail.com>
2020-04-23 16:23:51 +0300

    Added WorldMap plugin

---
+++ WorldMap/WorldMap/TerrainGenerator.cs
@@ -0,0 +1,273 @@
+﻿using System.Collections.Generic;
+using System;
+
+public class TerrainGenerator {
+    private float minimum;
+    private float maximum;
+    private System.Random random;
+
+    private WorldMap worldMap;
+
+    public TerrainGenerator(WorldMap worldMap) {
+        this.worldMap = worldMap;
+        this.minimum = -this.worldMap.worldLength / 2;
+        this.maximum = -1 * minimum;
+        this.random = new System.Random();
+    }
+
+    public void markNewEntity(Tuple<float, float, float> position, byte entityType) {
+        // TODO: must be moved on the server
+        int index = this.worldMap.getGridIndex(position.Item1, position.Item2, position.Item3);
+        //Debug.Log(position + " " + index);
+
+        int cell = this.worldMap.getCell(index);
+        if (this.worldMap.isFreeCell(cell)) {
+            this.worldMap.markCell(index, 0, entityType);
+        }
+    }
+
+    public float[,] generateRandomPositionedTrees(int numberOfTrees, int maxIter = 100) {
+        // TODO: must be moved on the server
+        float[,] positions = new float[numberOfTrees, 3];
+
+        for (int i = 0; i < numberOfTrees; i++) {
+            Tuple<float, float, float> position = null;
+
+            int iter = 0;
+            while (true) {
+                if (iter >= maxIter) {
+                    // Debug.Log("Cannot generate random tree. No more random positions!");
+                    return null;
+                }
+                position = getRandomPosition();
+
+                // TODO: check for grid integrity
+                int index = this.worldMap.getGridIndex(position.Item1, position.Item2, position.Item3);
+                if (this.worldMap.isFreeIndexCell(index)) {
+                    this.markNewEntity(position, 100);
+                    break;
+                }
+
+                iter++;
+            }
+
+            positions[i, 0] = position.Item1;
+            positions[i, 1] = position.Item2;
+            positions[i, 2] = position.Item3;
+        }
+
+        return positions;
+    }
+
+    public float[,] generateRandomForests(int numberOfTrees, int numberOfForests = 10, int maxIter = 100) {
+        float[,] positions = new float[numberOfTrees, 3];
+
+        // randomly choose between 5 and 9 forests
+        // int forestsNumber = this.random.Next(5, 10);
+        int averageForestSize = numberOfTrees / numberOfForests;
+
+        int[] forestSizes = new int[numberOfForests];
+
+        // randomly genereate forestNumber - 1 forest sizes
+        int totalNoTrees = numberOfTrees;
+        int lowerRandomLimit = (int)(averageForestSize / 1.5f);
+        int upperRandomLimit = (int)(averageForestSize * 1.3f);
+        int arrayLength = 0;
+
+        // generate a size for every forest
+        for (int i = 0; i < numberOfForests - 1; i++) {
+            int forestSize;
+            if (totalNoTrees < upperRandomLimit) {
+                forestSize = this.random.Next((int)(totalNoTrees / 1.3f), totalNoTrees);
+            } else {
+                forestSize = this.random.Next(lowerRandomLimit, upperRandomLimit);
+            }
+
+            if (totalNoTrees - forestSize <= 0) {
+                forestSizes[i] = totalNoTrees;
+                totalNoTrees -= forestSize;
+                Console.WriteLine("Out of trees");
+                break;
+            }
+
+            forestSizes[i] = forestSize;
+            totalNoTrees -= forestSize;
+            arrayLength++;
+
+            //Debug.Log("Forest size: " + forestSize);
+        }
+
+        // append the remaining trees to the last forest. Usually, this will be the largest one
+        if (totalNoTrees > 0) {
+            forestSizes[arrayLength] = totalNoTrees;
+            //Debug.Log("Last forest: " + totalNoTrees);
+        }
+
+        // start creating the last forest, as it is most probably the largest one
+        int treeIndex = 0;
+        Console.WriteLine("Actual number of forests: " + (arrayLength + 1));
+
+        for (int i = arrayLength; i >= 0; i--) {
+            int forestSize = forestSizes[i];
+            //Debug.Log("Forest size current: " + forestSize);
+            int addedUntilNow = 0;
+            Queue<int> queue = new Queue<int>();
+
+            int startIndex;
+            while (true) {
+                // TODO: force these indexes to be uniformly distributed over the map. Now, usually, they
+                // are scattered on some map quarter
+                startIndex = this.random.Next(0, this.worldMap.gridSize * this.worldMap.gridSize);
+
+                if (this.worldMap.isFreeIndexCell(startIndex)) {
+                    break;
+                }
+            }
+
+            queue.Enqueue(startIndex);
+
+            while (queue.Count > 0) {
+                int gridIndex = queue.Dequeue();
+
+                Tuple<float, float, float> treePos = this.worldMap.getCellPosition(gridIndex);
+
+                positions[treeIndex, 0] = treePos.Item1;
+                positions[treeIndex, 1] = treePos.Item2;
+                positions[treeIndex, 2] = treePos.Item3;
+
+                this.markNewEntity(treePos, 100);
+                treeIndex++;
+
+                addedUntilNow++;
+                if (addedUntilNow >= forestSize) {
+                    break;
+                }
+
+                int[] neighbours = {
+                    this.worldMap.getUpperCell(gridIndex),
+                    this.worldMap.getRightCell(gridIndex),
+                    this.worldMap.getLowerCell(gridIndex),
+                    this.worldMap.getLeftCell(gridIndex)
+                };
+
+                // add noise to the normal traverse for obtaining iregular looking forests. We'll simply ignore a random 
+                // chosen direction
+                int noiseIndex = this.random.Next(4);
+                neighbours[noiseIndex] = -1;
+
+                for (int j = 0; j < 4; j++) {
+                    if (neighbours[j] != -1 && this.worldMap.isFreeIndexCell(neighbours[j])) {
+                        queue.Enqueue(neighbours[j]);
+                    }
+                }
+            }
+
+        }
+
+        return positions;
+    }
+
+    public float[,] generateRandomMines(int noMines) {
+        /*
+         * A gold/stone mine will take 4 tiles:
+         *      |  |  |
+         *      |  |  |
+         * The mine's center will be in its center (viewing it from the game scene perspective)
+         * 
+         * CONVENTION: after a mine is exhausted, it will still be there, but as a dead one and still as 
+         * a navigation obstacle
+         */
+        float[,] minePositions = new float[noMines, 3];
+
+        for (int i = 0; i < noMines; i++) {
+            int index;
+            while (true) {
+                // TODO: force these indexes to be uniformly distributed over the map. Now, usually, they
+                // are scattered on some map quarter
+                index = this.random.Next(0, this.worldMap.gridSize * this.worldMap.gridSize);
+
+                if (this.worldMap.isFreeIndexCell(index)) {
+                    /*
+                     * | 1 | 2 | 3 |
+                     * | 4 | 5 | 6 |
+                     * | 7 | 8 | 9 | 
+                     */
+                    int top = this.worldMap.getUpperCell(index);
+                    int right = this.worldMap.getRightCell(index);
+                    int bottom = this.worldMap.getLowerCell(index);
+                    int left = this.worldMap.getLeftCell(index);
+
+                    Tuple<float, float, float> midPos = this.worldMap.getCellPosition(index);
+                    bool found = false;
+                    float centerX = 0, centerZ = 0;
+
+                    if (this.worldMap.isFreeIndexCell(top)) {
+                        if (this.worldMap.isFreeIndexCell(left)) {
+                            // 5 - 2 - 1- 4
+                            int topLeft = this.worldMap.getUpperCell(left);
+                            if (this.worldMap.isFreeIndexCell(topLeft)) {
+                                found = true;
+                                Tuple<float, float, float> leftPos = this.worldMap.getCellPosition(left);
+                                Tuple<float, float, float> topPos = this.worldMap.getCellPosition(top);
+                                centerX = (leftPos.Item1 + midPos.Item1) / 2;
+                                centerZ = (topPos.Item3 + midPos.Item3) / 2;
+                            }
+                        } else if (this.worldMap.isFreeIndexCell(right)) {
+                            // 5 - 2 - 3 - 6
+                            int topRight = this.worldMap.getUpperCell(right);
+                            if (this.worldMap.isFreeIndexCell(topRight)) {
+                                found = true;
+                                Tuple<float, float, float> rightPos = this.worldMap.getCellPosition(right);
+                                Tuple<float, float, float> topPos = this.worldMap.getCellPosition(top);
+                                centerX = (rightPos.Item1 + midPos.Item1) / 2;
+                                centerZ = (topPos.Item3 + midPos.Item3) / 2;
+                            }
+                        }
+                    } else if (this.worldMap.isFreeIndexCell(bottom)) {
+                        if (this.worldMap.isFreeIndexCell(left)) {
+                            // 5 - 8 - 7 - 4
+                            int bottomLeft = this.worldMap.getLowerCell(left);
+                            if (this.worldMap.isFreeIndexCell(bottomLeft)) {
+                                found = true;
+                                Tuple<float, float, float> leftPos = this.worldMap.getCellPosition(left);
+                                Tuple<float, float, float> bottomPos = this.worldMap.getCellPosition(bottom);
+                                centerX = (leftPos.Item1 + midPos.Item1) / 2;
+                                centerZ = (bottomPos.Item3 + midPos.Item3) / 2;
+                            }
+                        } else if (this.worldMap.isFreeIndexCell(right)) {
+                            // 5 - 6 - 8 - 9
+                            int bottomRight = this.worldMap.getLowerCell(right);
+                            if (this.worldMap.isFreeIndexCell(bottomRight)) {
+                                found = true;
+                                Tuple<float, float, float> rightPos = this.worldMap.getCellPosition(right);
+                                Tuple<float, float, float> bottomPos = this.worldMap.getCellPosition(bottom);
+                                centerX = (rightPos.Item1 + midPos.Item1) / 2;
+                                centerZ = (bottomPos.Item3 + midPos.Item3) / 2;
+                            }
+                        }
+                    }
+
+                    if (found) {
+                        minePositions[i, 0] = centerX;
+                        minePositions[i, 1] = midPos.Item2;
+                        minePositions[i, 2] = centerZ;
+
+                        this.markNewEntity(new Tuple<float, float, float>(centerX, midPos.Item2, centerZ), 101);
+                        break;
+                    }
+                }
+            }
+        }
+
+        return minePositions;
+    }
+
+    private Tuple<float, float, float> getRandomPosition() {
+        // TODO: must be moved on the server
+        float x = (float)(this.random.NextDouble()) * (this.maximum - this.minimum) + this.minimum;
+        float y = 0;
+        float z = (float)(this.random.NextDouble()) * (this.maximum - this.minimum) + this.minimum;
+
+        return new Tuple<float, float, float>(x, y, z);
+    }
+}
---
+++ WorldMap/WorldMap/WorldMap.cs
@@ -0,0 +1,201 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+/*
+ * CONVENTIONS:
+ * 1. the map is centered in (0, 0, 0)
+ * 2. the map has square form
+ */
+public class WorldMap {
+    public ushort worldLength;
+    public float cellLength;
+    public ushort gridSize;
+
+    public static int keyCounter = 1;
+
+    private int[] world;
+    private float halfCellLength;
+    private ushort halfWorldLength;
+    private int halfGridSize;
+
+    public WorldMap(ushort worldLength = 1024, float cellLength = 0.5f) {
+        this.worldLength = worldLength;
+        this.cellLength = cellLength;
+
+        this.gridSize = (ushort)(this.worldLength / this.cellLength);
+        this.world = new int[this.gridSize * this.gridSize];
+        this.halfCellLength = this.cellLength / 2;
+        this.halfWorldLength = (ushort)(this.worldLength / 2);
+        this.halfGridSize = this.gridSize / 2;
+    }
+
+    public void buildGridFromTerrain() {
+
+    }
+
+    public Tuple<float, float> getCellCenter(float x, float y, float z) {
+        Tuple<float, float> result;
+
+        // TODO: might need to invert these if you rotate the world
+        int wholePartX = (int)x;
+        int wholePartZ = (int)z;
+
+        float moveX = this.halfCellLength;
+        float moveZ = this.halfCellLength;
+
+        if (wholePartX < 0) {
+            moveX *= -1;
+        }
+
+        if (wholePartZ < 0) {
+            moveZ *= -1;
+        }
+
+        result = new Tuple<float, float>(wholePartX + moveX, wholePartZ + moveZ);
+
+        return result;
+    }
+
+    public Tuple<float, float, float> getCellPosition(int index) {
+        int line = index / this.gridSize;
+        int col = index % this.gridSize;
+
+        float x, y, z;
+
+        // TODO: change this if you'll add terrain height
+        y = 0;
+
+        //Debug.Log("Half: " + this.halfWorldLength + " " + this.halfGridSize + " " + this.gridSize);
+
+        if (line <= this.halfGridSize) {
+            // Z coord will be positive
+            z = this.halfWorldLength - (line * this.cellLength + this.halfCellLength);
+        } else {
+            // Z coord will be negative
+            z = this.halfWorldLength - (line * this.cellLength + this.halfCellLength);
+        }
+
+        if (col <= this.halfGridSize) {
+            // X coord will be negative
+            x = -(col * this.cellLength - this.halfCellLength);
+        } else {
+            // X coord will be positive
+            x = (col * this.cellLength + this.halfCellLength) - this.halfWorldLength;
+        }
+
+        return new Tuple<float, float, float>(x, y, z);
+    }
+
+    public int getGridIndex(float x, float y, float z) {
+        // the Y coord will be ignored, it's here only as a convention
+        int index = 0;
+
+        // check for out of world coordinates
+        if (x > this.halfWorldLength || x < -this.halfWorldLength ||
+            z > this.halfWorldLength || x < -this.halfWorldLength) {
+
+            return -1;
+        }
+
+        // the normal case
+        // TODO: might need to invert these if you rotate the world
+        float wholePartX;
+        float wholePartZ;
+
+        if (x < 0) {
+            wholePartX = Math.Abs(x - this.halfCellLength);
+        } else {
+            wholePartX = x + this.halfWorldLength;
+        }
+
+        if (z < 0) {
+            wholePartZ = Math.Abs(z) + this.halfWorldLength;
+        } else {
+            wholePartZ = this.halfWorldLength - z;
+        }
+
+        int line = (int)(wholePartZ / this.cellLength);
+        int col = (int)(wholePartX / this.cellLength);
+
+        index = line * this.gridSize + col;
+
+        return index;
+    }
+
+    public int getUpperCell(int index) {
+        int line = index / this.gridSize;
+        int col = index % this.gridSize;
+
+        if (line - 1 < 0) {
+            return -1;
+        }
+
+        return (line - 1) * this.gridSize + col;
+    }
+
+    public int getLeftCell(int index) {
+        int line = index / this.gridSize;
+        int col = index % this.gridSize;
+
+        if (col - 1 < 0) {
+            return -1;
+        }
+
+        return line * this.gridSize + (col - 1);
+    }
+
+    public int getRightCell(int index) {
+        int line = index / this.gridSize;
+        int col = index % this.gridSize;
+
+        if (col + 1 >= this.gridSize) {
+            return -1;
+        }
+
+        return line * this.gridSize + (col + 1);
+    }
+
+    public int getLowerCell(int index) {
+        int line = index / this.gridSize;
+        int col = index % this.gridSize;
+
+        if (line + 1 >= this.gridSize) {
+            return -1;
+        }
+
+        return (line + 1) * this.gridSize + col;
+    }
+
+    public int getCell(int index) {
+        return this.world[index];
+    }
+
+    public int getPlayer(int cell) {
+        return cell >> 24;
+    }
+
+    /*public ushort getEntityType(ushort cell) {
+        return (ushort)((cell << 8) >> 8);
+    }*/
+
+    public int buildCell(int player, int id) {
+        return player | id;
+    }
+
+    public void markCell(int index, int player, int entityId) {
+        this.world[index] = this.buildCell(player, entityId);
+    }
+
+    public bool isFreeIndexCell(int index) {
+        if (index < 0) {
+            return false;
+        }
+
+        return this.getCell(index) == 0;
+    }
+
+    public bool isFreeCell(int cell) {
+        return cell == 0;
+    }
+}
